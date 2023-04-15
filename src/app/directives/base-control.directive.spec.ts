57e3e92505fffd1ab016510dcb9b6f70fa4d182c
EvgenYakov <pol96956@gmail.com>
2023-04-15 21:24:42 +0300

    add footer to index.ts

---
+++ src/app/directives/base-control.directive.spec.ts
@@ -0,0 +1,8 @@
+import { BaseControl } from './base.control';
+
+describe('BaseControlDirective', () => {
+  it('should create an instance', () => {
+    const directive = new BaseControl();
+    expect(directive).toBeTruthy();
+  });
+});
