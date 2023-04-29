6b32ed003a98640879888a5371f4bc429b3c7d11
EvgenYakov <pol96956@gmail.com>
2023-04-29 18:50:25 +0200

    feat: change filter and add to start page

--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -2,12 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FooterModule, HeaderModule } from '@layout';
+import { AutoFilterModule } from '@components';
 import { MainRoutingModule } from './main-routing.module';
 import { MainComponent } from './main.component';
 import { StartPageComponent } from './components/start-page/start-page.component';
 
 @NgModule({
   declarations: [MainComponent, StartPageComponent],
-  imports: [CommonModule, MainRoutingModule, FooterModule, HeaderModule],
+  imports: [
+    CommonModule,
+    MainRoutingModule,
+    FooterModule,
+    HeaderModule,
+    AutoFilterModule,
+  ],
 })
 export class MainModule {}
