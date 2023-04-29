a624110e74c387aa3aaec722edbc2f2a6c5fcbc3
Roman Degtyarev <wize.arbik@gmail.com>
2023-04-29 13:11:45 +0200

    feat: change footer

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -2,10 +2,8 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 import {
   AUTOPARK_COLUMN,
-  COPYRIGHT_TEXT,
   DRIVER_COLUMN,
   INFO_COLUMN,
-  LEGAL_SERVICE_COLUMN,
 } from './constants/footer.constant';
 
 @Component({
@@ -18,6 +16,4 @@ export class FooterComponent {
   public driver = DRIVER_COLUMN;
   public autopark = AUTOPARK_COLUMN;
   public info = INFO_COLUMN;
-  public legalService = LEGAL_SERVICE_COLUMN;
-  public copyrightText = COPYRIGHT_TEXT;
 }
