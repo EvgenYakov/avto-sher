1fd1c298434242ccc91d84f3e6c64c7dfdb7beca
EvgenYakov <pol96956@gmail.com>
2023-04-29 13:11:45 +0200

    feat: change footer

--- src/app/layout/footer/constants/footer.constant.ts
+++ src/app/layout/footer/constants/footer.constant.ts
@@ -2,50 +2,36 @@ import { MenuItem } from 'primeng/api';
 
 export const DRIVER_COLUMN: MenuItem[] = [
   {
-    label: 'Как сделать заказ?'
+    label: 'Как сделать заказ?',
   },
   {
-    label: 'Оплата'
+    label: 'Полное руководство',
   },
-  {
-    label: 'Полное руководство'
-  }
 ];
 
 export const AUTOPARK_COLUMN: MenuItem[] = [
   {
-    label: 'Как добавить свой автопарк'
+    label: 'Как добавить свой автопарк',
   },
   {
-    label: 'Полное руководство'
+    label: 'Полное руководство',
   },
   {
-    label: 'Условия размещения'
+    label: 'Условия размещения',
   },
   {
-    label: 'Добавить свой автопарк'
-  }
+    label: 'Добавить свой автопарк',
+  },
 ];
 
 export const INFO_COLUMN: MenuItem[] = [
   {
-    label: 'Обратная связь'
+    label: 'Обратная связь',
   },
   {
-    label: 'Правила'
+    label: 'Правила',
   },
   {
-    label: 'Команда сайта'
+    label: 'Команда сайта',
   },
-  {
-    label: 'Вакансии'
-  }
 ];
-
-export const LEGAL_SERVICE_COLUMN: MenuItem[] = [
-  {
-    label: 'Обратная связь'
-  }
-];
-
-export const COPYRIGHT_TEXT = 'Copyright © 2023 ЯАВТОРФ. Все права защищены';
