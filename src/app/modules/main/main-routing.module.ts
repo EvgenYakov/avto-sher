9ace991603df785532283c9557652603e5e351f2
Roman Degtyarev <wize.arbik@gmail.com>
2023-04-29 18:50:25 +0200

    feat: change filter and add to start page

--- src/app/modules/main/main-routing.module.ts
+++ src/app/modules/main/main-routing.module.ts
@@ -14,6 +14,7 @@ const routes: Routes = [
         component: StartPageComponent,
       },
       {
+        //for test
         path: 'auto-detailed',
         loadChildren: () =>
           import('../../modules/auto-detailed/auto-detailed.module').then(
