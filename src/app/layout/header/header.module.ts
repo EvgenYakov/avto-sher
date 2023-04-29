9235cb2c4763c54743d53b90f7be16bee04ade62
EvgenYakov <pol96956@gmail.com>
2023-04-29 11:10:05 +0200

    feat: add main module

--- src/app/layout/header/header.module.ts
+++ src/app/layout/header/header.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { InputTextModule } from 'primeng/inputtext';
 import { ToolbarModule } from 'primeng/toolbar';
@@ -18,7 +17,6 @@ import { CustomNavbarComponent } from './components/custom-navbar/custom-navbar.
     InputTextModule,
     ReactiveFormsModule,
     RouterLink,
-    BrowserAnimationsModule,
     ToolbarModule,
   ],
 })
---
+++ src/app/modules/main/main.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FooterModule, HeaderModule } from '@layout';
+import { MainRoutingModule } from './main-routing.module';
+import { MainComponent } from './main.component';
+import { StartPageComponent } from './components/start-page/start-page.component';
+
+@NgModule({
+  declarations: [MainComponent, StartPageComponent],
+  imports: [CommonModule, MainRoutingModule, FooterModule, HeaderModule],
+})
+export class MainModule {}
