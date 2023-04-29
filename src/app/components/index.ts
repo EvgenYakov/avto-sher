d91a5ed4b15e0284b89458c00b805d07b53a16fb
EvgenYakov <pol96956@gmail.com>
2023-04-29 22:45:37 +0200

    fix: delete components from start page

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -7,4 +7,7 @@ export * from './review-autopark-card/review-autopark-card.module';
 export * from './auto-filter/auto-filter.component';
 export * from './auto-filter/auto-filter.module';
 
+export * from './autopark-card/autopark-card.component';
+export * from './autopark-card/autopark-card.module';
+
 export * from './controls';
