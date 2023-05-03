35e1b926e7a9e5a203db7f4e758914e8cdd21868
Nikolai <mikalai.nahornykh@innowise-group.com>
2023-05-03 17:20:15 +0300

    change auto card styles, create load-more component, add list cards to autopark profile

--- src/app/components/autopark-card/constants/autopar.constant.ts
+++ src/app/components/autopark-card/constants/autopar.constant.ts
@@ -1,7 +1,7 @@
 import { AutoparkCard } from '@models';
 
 export const autoparkCard: AutoparkCard = {
-  avatarPath: 'assets/test-img.jpg',
+  logo: 'assets/test-img.jpg',
   autoparkName: 'Быстрее ветра',
   autoCount: 165,
   ratingCount: 4.3,
