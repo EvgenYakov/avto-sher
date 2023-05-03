db58dba48342460d9c10f9ed7202386fd6531acf
EvgenYakov <pol96956@gmail.com>
2023-05-03 12:32:14 +0200

    feat: change user-avatar logic

--- src/app/modules/user-profile/user-profile.module.ts
+++ src/app/modules/user-profile/user-profile.module.ts
@@ -7,9 +7,15 @@ import { UserProfileRoutingModule } from './user-profile-routing.module';
 import { UserProfileComponent } from './user-profile.component';
 import { CardButtonComponent } from './components/card-button/card-button.component';
 import { CardLinkComponent } from './components/card-link/card-link.component';
+import { UserAvatarComponent } from './components/user-avatar/user-avatar.component';
 
 @NgModule({
-  declarations: [UserProfileComponent, CardButtonComponent, CardLinkComponent],
+  declarations: [
+    UserProfileComponent,
+    CardButtonComponent,
+    CardLinkComponent,
+    UserAvatarComponent,
+  ],
   imports: [CommonModule, UserProfileRoutingModule, CardModule],
 })
 export class UserProfileModule {}
