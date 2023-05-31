e09174ddf9d419d723538c91fd3e0c41ec0d15e2
EvgenYakov <pol96956@gmail.com>
2023-05-31 16:54:14 +0300

    delete hard-code

--- src/app/modules/main/main-routing.module.ts
+++ src/app/modules/main/main-routing.module.ts
@@ -38,7 +38,7 @@ const routes: Routes = [
           ),
       },
       {
-        path: AppRoutes.AUTOPARK_DETAILED+'/:id',
+        path: AppRoutes.AUTOPARK_DETAILED + '/:id',
         loadChildren: () =>
           import('../autopark-detailed/autopark-detailed.module').then(
             (m) => m.AutoparkDetailedModule
