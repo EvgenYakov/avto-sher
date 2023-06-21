e1b59cce1ffd8077774888d1254596471558af72
Nikolai <niko1ai_nagornyh@mail.ru>
2023-06-21 17:37:30 +0300

    delete extra action call

--- src/app/pages/start-page/start-page.component.ts
+++ src/app/pages/start-page/start-page.component.ts
@@ -2,12 +2,13 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 
-import { selectAuctionAutoparks, selectLoading } from '@store';
-import { AppRoutes, LoadingTypes, MainRoutes } from '@constants';
+import { selectAuctionAutoparks, selectLoading, selectRegion } from '@store';
+import { AppRoutes, LoadingTypes, LocalStorageKeys, MainRoutes } from '@constants';
 
 import { START_PAGE_DEPS } from './start-page.dependencies';
 import { Observable } from 'rxjs';
 import { AuctionAutoparks } from '@models';
+import { LocalStorageService } from '@services';
 
 @Component( {
   selector: 'app-start-page',
@@ -21,7 +22,8 @@ export class StartPageComponent implements OnInit {
 
   constructor(
     private store: Store,
-    private router: Router
+    private router: Router,
+    private localStorageService: LocalStorageService
   ) {}
 
   public auctionAutoparks$: Observable<AuctionAutoparks>;
@@ -29,6 +31,10 @@ export class StartPageComponent implements OnInit {
 
   ngOnInit(): void {
     this.getDataFromStore();
+    if(this.localStorageService.getItemFromStorage( LocalStorageKeys.REGION )) {
+      const region = this.localStorageService.getItemFromStorage( LocalStorageKeys.REGION );
+      this.store.dispatch( selectRegion( { region } ) )
+    }
   }
 
   public navigateToDetailed(autoparkId: number): void {
@@ -38,5 +44,6 @@ export class StartPageComponent implements OnInit {
   private getDataFromStore(): void {
     this.auctionAutoparks$ = this.store.select( selectAuctionAutoparks );
     this.isLoading$ = this.store.select( selectLoading, { type: LoadingTypes.AUTOPARKS } );
+
   }
 }
