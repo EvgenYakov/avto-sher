964f9be1dc74789315938f5893b5011c7eca3621
Nikolai <niko1ai_nagornyh@mail.ru>
2023-06-29 19:15:01 +0300

    add links to social networks

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,7 +1,9 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
-import { AUTOPARK_COLUMN, DRIVER_COLUMN, INFO_COLUMN, } from './constants/footer-navigation.constants';
+import { AUTOPARK_COLUMN, DRIVER_COLUMN, INFO_COLUMN, SOCIAL_NETWORK, } from './constants/footer-navigation.constants';
 import { FOOTER_CARD_DEPS } from './footer.dependencies';
+import { HoverImageDirective } from './hover-image/hover-image.directive';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-footer',
@@ -9,10 +11,11 @@ import { FOOTER_CARD_DEPS } from './footer.dependencies';
     styleUrls: ['./footer.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
     standalone: true,
-    imports: [FOOTER_CARD_DEPS]
+  imports: [FOOTER_CARD_DEPS, HoverImageDirective, RouterLink]
 })
 export class FooterComponent {
   public driverNav = DRIVER_COLUMN;
   public autoparkNav = AUTOPARK_COLUMN;
   public infoNav = INFO_COLUMN;
+  public socialNetworks = SOCIAL_NETWORK;
 }
--- src/app/pages/auto-detailed/constants/responsive-options.constants.ts
+++ src/app/pages/auto-detailed/constants/responsive-options.constants.ts
@@ -2,6 +2,10 @@ import { ResponsiveOption } from '../interfaces';
 
 export const RESPONSIVE_OPTIONS: ResponsiveOption[] = [
   {
+    breakpoint: '1280px',
+    numVisible: 3,
+  },
+  {
     breakpoint: '768px',
     numVisible: 3,
   },
