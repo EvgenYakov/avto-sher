c5b4e50d52b68370929ea385dc7acd99a845d2b3
Nikolai <niko1ai_nagornyh@mail.ru>
2023-07-12 19:44:48 +0300

    add empty dropdown to header

--- src/app/pages/control-panel/control-panel.component.ts
+++ src/app/pages/control-panel/control-panel.component.ts
@@ -6,13 +6,14 @@ import { SIDEBAR_CONFIG } from './constants/sidebar-config.constant';
 import { CONTROL_PANEL_DEPS } from './control-panel.dependencies';
 import { SidebarConfig } from './models/sidebar-config.interface';
 import { Title } from '@angular/platform-browser';
+import { DropdownModule } from 'primeng/dropdown';
 
 @Component( {
   selector: 'app-control-panel',
   standalone: true,
   templateUrl: './control-panel.component.html',
   styleUrls: ['./control-panel.component.scss'],
-  imports: [CONTROL_PANEL_DEPS],
+  imports: [CONTROL_PANEL_DEPS, DropdownModule],
 } )
 export class ControlPanelComponent implements OnInit {
   public sidebarVisible = false;
