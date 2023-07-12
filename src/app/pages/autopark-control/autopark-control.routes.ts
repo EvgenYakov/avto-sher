a3e29e9d0c5cf3153bce560884a7204761bdbe11
EvgenYakov <pol96956@gmail.com>
2023-07-12 11:05:28 +0300

    add header to control panel

--- src/app/pages/autopark-control/autopark-control.routes.ts
+++ src/app/pages/autopark-control/autopark-control.routes.ts
@@ -2,8 +2,8 @@ import { Route } from '@angular/router';
 
 import { ControlPanel } from '@constants';
 
-import { AutoparkControlComponent } from './autopark-control.component';
 import { CreateCarComponent } from './components';
+import { AutoparkControlComponent } from './autopark-control.component';
 import { CarListComponent } from './components/car-list/car-list.component';
 
 export default [
@@ -12,10 +12,14 @@ export default [
     component: AutoparkControlComponent,
     children: [
       {
-        path: ControlPanel.CARS_TABLE, component: CarListComponent
+        path: ControlPanel.CARS_TABLE,
+        title: 'Машины',
+        component: CarListComponent
       },
       {
-        path: ControlPanel.CREATE_CAR, component: CreateCarComponent
+        path: ControlPanel.CREATE_CAR,
+        title: 'Добавление авто',
+        component: CreateCarComponent
       },
     ]
   }
