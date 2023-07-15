77b5aeaef73acbe264fef118a35cc6fd809fa9fe
EvgenYakov <pol96956@gmail.com>
2023-07-15 15:51:00 +0300

    completed create-car template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,14 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { Router, RouterOutlet } from '@angular/router';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
-import { LocalStorageService } from '@services';
-import { AppRoutes, LocalStorageKeys, UserRole } from '@constants';
-
-@Component( {
+@Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
   imports: [RouterOutlet],
   changeDetection: ChangeDetectionStrategy.OnPush,
-} )
-export class AppComponent implements OnInit {
-  constructor(
-    private localStorageService: LocalStorageService,
-    private router: Router
-  ) {}
+})
+export class AppComponent {
 
-  ngOnInit(): void {
-    const role = this.localStorageService.getItemFromStorage(LocalStorageKeys.ROLE);
-    switch (role) {
-      case UserRole.DRIVER:
-        this.router.navigate(['/' + AppRoutes.MAIN]);
-        break;
-      case UserRole.OWNER:
-        this.router.navigate(['/' + AppRoutes.AUTOPARK_PANEL]);
-        break;
-    }
-  }
 }
