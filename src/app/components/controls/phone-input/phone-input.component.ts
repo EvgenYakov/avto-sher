2876d230e128eae2a3bad311563a35f97467dc31
EvgenYakov <pol96956@gmail.com>
2024-03-23 22:31:06 +0300

    updated imports

--- src/app/components/controls/phone-input/phone-input.component.ts
+++ src/app/components/controls/phone-input/phone-input.component.ts
@@ -2,10 +2,10 @@ import { NgClass } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { BaseControlDirective } from '@models';
 import { InputMaskModule } from 'primeng/inputmask';
 
 import { ControlWrapperComponent } from '../control-wrapper/control-wrapper.component';
+import { BaseControlDirective } from 'src/app/models/classes/base-control.directive';
 
 @Component({
   selector: 'app-phone-input',
--- src/app/components/controls/text-input/text-input.component.ts
+++ src/app/components/controls/text-input/text-input.component.ts
@@ -2,10 +2,10 @@ import { NgClass } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { BaseControlDirective } from '@models';
 import { InputTextModule } from 'primeng/inputtext';
 
 import { ControlWrapperComponent } from '../control-wrapper/control-wrapper.component';
+import { BaseControlDirective } from 'src/app/models/classes/base-control.directive';
 
 @Component({
   selector: 'app-text-input',
