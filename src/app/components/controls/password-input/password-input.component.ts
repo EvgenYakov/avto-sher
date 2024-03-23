dc372a8053dca1965de4fb944be1e1638d24888a
Nikolai Nagornykh <72821850+Metiarizmus@users.noreply.github.com>
2024-03-23 22:31:06 +0300

    updated imports

--- src/app/components/controls/password-input/password-input.component.ts
+++ src/app/components/controls/password-input/password-input.component.ts
@@ -2,10 +2,10 @@ import { NgClass } from '@angular/common';
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { BaseControlDirective } from '@models';
 import { PasswordModule } from 'primeng/password';
 
 import { ControlWrapperComponent } from '../control-wrapper/control-wrapper.component';
+import { BaseControlDirective } from 'src/app/models/classes/base-control.directive';
 
 @Component({
   selector: 'app-password-input',
